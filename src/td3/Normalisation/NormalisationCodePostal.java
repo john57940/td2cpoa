cbaf99cf0f537ae68cd3ba81551d8051ee5cc270
vernet18u <vernet18u@iutm-inf-f36-01.iutmetz.site.univ-lorraine.fr>
2021-10-08 11:55:48 +0200

    codepostal

--- src/td3/Normalisation/NormalisationCodePostal.java
+++ src/td3/Normalisation/NormalisationCodePostal.java
@@ -1,14 +1,18 @@
 package td3.Normalisation;
 
 public class NormalisationCodePostal {
-//	public static String normalize (String code) {
-//		   code = code.replaceAll("\\s+","");
-  //     int StringLength=code.length();
-//	if(StringLength=4) {
- //   	   String res = "0"+code;
+	public static String normalize (String code) {
+	   code = code.replaceAll("\\s+","");
+      int StringLength=code.length();
+	if(StringLength==4) {
+    	   return "0"+code;
        }
-//	else if(StringLength=6) {
- //	   String res = code.deleteCharAt(0,1);
- //  }  
-		//}
-//}
+	else if(StringLength==6) {
+    return code.substring(2) ;
+  }  
+	else
+	{
+		return code;      //on part du principe que les gens écrivent des codes valides donc entre 4 et 6 caractères
+	}
+		}
+}
